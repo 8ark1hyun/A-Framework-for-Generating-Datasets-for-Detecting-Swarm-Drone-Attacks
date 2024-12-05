a56a597c492dc3fcd9acb66ffae19db1a52e896b
8ark1hyun <kihyun@postech.ac.kr>
2024-12-05 15:54:37 +0900

    MODIFY: leader-follower.py

--- CSED499I_COMMAND/leader-follower.py
+++ CSED499I_COMMAND/leader-follower.py
@@ -3,16 +3,25 @@ import math
 from pymavlink import mavutil
 
 def drone_connect(udp_port):
-    print(f"Connecting to drone on UDP port {udp_port}...")
+    if udp_port == 14540:
+        print("Connecting to Leader Drone...")
+    elif udp_port > 14540:
+        print("Connecting to Follower Drone...")
     drone = mavutil.mavlink_connection(f'udp:localhost:{udp_port}')
 
     drone.wait_heartbeat()
-    print(f"Drone on port {udp_port} connected!")
+    if udp_port == 14540:
+        print("Leader Drone Connected!")
+    elif udp_port > 14540:
+        print("Follower Drone Connected!")
 
     return drone
-        
+
 def drone_arm(drone, target_system):
-    print(f"[System ID: {target_system}] Arming drone...")
+    if target_system == 1:
+        print("Arming Leader Drone...")
+    elif target_system > 1:
+        print("Arming Follower Drone...")
     drone.mav.command_long_send(
         target_system,
         0,
@@ -21,12 +30,18 @@ def drone_arm(drone, target_system):
         1, 0, 0, 0, 0, 0, 0
     )
     drone.motors_armed_wait()
-    print(f"[System ID: {target_system}] Drone Armed!")
+    if target_system == 1:
+        print("Leader Drone Armed!")
+    elif target_system > 1:
+        print("Follower Drone Armed!")
 
     time.sleep(5)
-    
+
 def drone_disarm(drone, target_system):
-    print(f"[System ID: {target_system}] Disarming drone...")
+    if target_system == 1:
+        print("Disarming Leader Drone...")
+    elif target_system > 1:
+        print("Disarming Follower Drone...")
     drone.mav.command_long_send(
         target_system,
         0,
@@ -34,12 +49,15 @@ def drone_disarm(drone, target_system):
         0,
         0, 0, 0, 0, 0, 0, 0
     )
-    print(f"[System ID: {target_system}] Drone Disarmed!")
+    if target_system == 1:
+        print("Leader Drone Disarmed!")
+    elif target_system > 1:
+        print("Follower Drone Disarmed!")
 
     time.sleep(5)
 
 def follower_set_mode(drone, target_system):
-    print("Setting Follower Drone Mode to OFFBOARD")
+    print("Setting Follower Drone's Mode to OFFBOARD...")
     drone.mav.command_long_send(
         target_system,
         0,
@@ -48,6 +66,7 @@ def follower_set_mode(drone, target_system):
         1,
         6, 0, 0, 0, 0, 0
     )
+    print("Setting Follower Drone's Mode Completed!")
 
 def drone_position(drone):
     msg = drone.recv_match(type='GLOBAL_POSITION_INT', blocking=True)
@@ -59,11 +78,11 @@ def drone_position(drone):
     return None, None, None
 
 def drone_takeoff(drone, target_system):
-    print(f"[System ID: {target_system}] Taking off...")
+    if target_system == 1:
+        print("Taking off Leader Drone...")
+    elif target_system > 1:
+        print("Taking off Follower Drone...")
     current_position = drone_position(drone)
-    # lat = current_position[0]
-    # lon = current_position[1]
-    # target_alt = current_position[2] + 10
 
     drone.mav.command_long_send(
         target_system,
@@ -72,7 +91,6 @@ def drone_takeoff(drone, target_system):
         0,
         0, 0, 0, 0, 36.010550, 129.321334, 10
     )
-    print(f"[System ID: {target_system}] Takeoff!")
 
     current_position = drone_position(drone)
     while True:
@@ -80,15 +98,19 @@ def drone_takeoff(drone, target_system):
         if abs(current_position[2] - 10) > 1:
             continue
         else:
+            if target_system == 1:
+                print("Leader Drone Took off!")
+            elif target_system > 1:
+                print("Follower Drone Took off!")
             time.sleep(2)
             return
 
 def drone_land(drone, target_system):
-    print(f"[System ID: {target_system}] Landing...")
+    if target_system == 1:
+        print("Landing Leader Drone...")
+    elif target_system > 1:
+        print("Landing Follower Drone...")
     current_position = drone_position(drone)
-    lat = current_position[0]
-    lon = current_position[1]
-    target_alt = 0
 
     drone.mav.command_long_send(
         target_system,
@@ -97,7 +119,6 @@ def drone_land(drone, target_system):
         0,
         0, 0, 0, 0, lat, lon, target_alt
     )
-    print(f"[System ID: {target_system}] Land!")
 
     current_position = drone_position(drone)
     while True:
@@ -105,48 +126,23 @@ def drone_land(drone, target_system):
         if abs(current_position[2] - 0) > 1:
             continue
         else:
+            if target_system == 1:
+                print("Leader Drone Landed!")
+            elif target_system > 1:
+                print("Follower Drone Landed!")
             time.sleep(2)
             return
-
-def calculate_distance_and_bearing(lat1, lon1, lat2, lon2):
-    R = 6371e3
-    lat1_rad = math.radians(lat1)
-    lon1_rad = math.radians(lon1)
-    lat2_rad = math.radians(lat2)
-    lon2_rad = math.radians(lon2)
-
-    dlat = lat2_rad - lat1_rad
-    dlon = lon2_rad - lon1_rad
-
-    a = math.sin(dlat / 2) ** 2 + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin(dlon / 2) ** 2
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-
-    distance = R * c
-    bearing = math.degrees(math.atan2(
-        math.sin(dlon) * math.cos(lat2_rad),
-        math.cos(lat1_rad) * math.sin(lat2_rad) - math.sin(lat1_rad) * math.cos(lat2_rad) * math.cos(dlon)
-    ))
-
-    return distance, (bearing + 360) % 360
 
 def calculate_position(leader_lat, leader_lon, leader_alt, distance):
     R = 6371e3
     dlat = distance / R
-    #dlon = distance / (R * math.cos(math.pi * leader_lat / 180))
 
     target_lat = leader_lat - (dlat * 180 / math.pi)
     target_lon = leader_lon
     target_alt = leader_alt
     return target_lat, target_lon, target_alt
 
-def smooth_velocity(distance, target_distance, max_speed):
-    if distance <= target_distance:
-        return 0
-    error = distance - target_distance
-    velocity = max_speed * min(error / target_distance, 1)
-    return velocity
-
-def send_velocity(follower, lat, lon, alt):
+def send_command(follower, lat, lon, alt):
     follower.mav.set_position_target_global_int_send(
         0,
         2,
@@ -171,22 +167,12 @@ def main():
 
     follower_set_mode(follower, target_system=2)
 
-    desired_distance = 2.0
-    max_speed = 10.0
-
     while True:
         leader_position = drone_position(leader)
-        follower_position = drone_position(follower)
 
-        # distance, bearing = calculate_distance_and_bearing(leader_position[0], leader_position[1], follower_position[0], follower_position[1])
+        target_lat, target_lon, target_alt = calculate_position(leader_position[0], leader_position[1], leader_position[2], distance=2.0)
 
-        # speed = smooth_velocity(distance, desired_distance, max_speed)
-        # vx = speed * math.cos(math.radians(bearing))
-        # vy = speed * math.sin(math.radians(bearing))
-
-        target_lat, target_lon, target_alt = calculate_position(leader_position[0], leader_position[1], leader_position[2], desired_distance)
-
-        send_velocity(follower, target_lat, target_lon, target_alt)
+        send_command(follower, target_lat, target_lon, target_alt)
 
         time.sleep(0.1)
 
--- CSED499I_COMMAND/leader.py
+++ CSED499I_COMMAND/leader.py
@@ -1,90 +1,126 @@
 # Leader Drone
 
 import time
+import math
 from pymavlink import mavutil
 
 def drone_connect(udp_port):
-    print("Connecting to Leader Drone...")
+    if udp_port == 14540:
+        print("Connecting to Leader Drone...")
+    elif udp_port > 14540:
+        print("Connecting to Follower Drone...")
     drone = mavutil.mavlink_connection(f'udp:localhost:{udp_port}')
 
     drone.wait_heartbeat()
-    print("Leader Drone Connected!")
+    if udp_port == 14540:
+        print("Leader Drone Connected!")
+    elif udp_port > 14540:
+        print("Follower Drone Connected!")
 
     return drone
 
-def follower_connect(udp_port):
-    print("Connecting to Follower Drone...")
-    follower = mavutil.mavlink_connection(f'udpout:localhost:{udp_port}')
-    print("Follower Drone Connected!")
-
-    return follower
-        
-def drone_arm(drone, follower):
-    print("Arming Leader drone...")
+def drone_arm(drone, target_system):
+    if target_system == 1:
+        print("Arming Leader Drone...")
+    elif target_system > 1:
+        print("Arming Follower Drone...")
     drone.mav.command_long_send(
-        1,
+        target_system,
         0,
         mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
         0,
         1, 0, 0, 0, 0, 0, 0
     )
     drone.motors_armed_wait()
-    print("Leader Drone Armed!")
+    if target_system == 1:
+        print("Leader Drone Armed!")
+    elif target_system > 1:
+        print("Follower Drone Armed!")
 
     time.sleep(5)
-    
-def drone_disarm(drone):
-    print("Disarming Leader drone...")
+
+def drone_disarm(drone, target_system):
+    if target_system == 1:
+        print("Disarming Leader Drone...")
+    elif target_system > 1:
+        print("Disarming Follower Drone...")
     drone.mav.command_long_send(
-        1,
+        target_system,
         0,
         mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
         0,
         0, 0, 0, 0, 0, 0, 0
     )
-    print("Leader Drone Disarmed!")
+    if target_system == 1:
+        print("Leader Drone Disarmed!")
+    elif target_system > 1:
+        print("Follower Drone Disarmed!")
 
     time.sleep(5)
+
+def follower_set_mode(drone, target_system):
+    print("Setting Follower Drone's Mode to OFFBOARD...")
+    drone.mav.command_long_send(
+        target_system,
+        0,
+        mavutil.mavlink.MAV_CMD_DO_SET_MODE,
+        0,
+        1,
+        6, 0, 0, 0, 0, 0
+    )
+    print("Setting Follower Drone's Mode Completed!")
 
 def drone_position(drone):
     msg = drone.recv_match(type='GLOBAL_POSITION_INT', blocking=True)
     if msg:
         latitude = msg.lat / 1e7
         longitude = msg.lon / 1e7
-        altitude = msg.relative_alt / 1000.0
+        altitude = msg.relative_alt / 1e3
         return latitude, longitude, altitude
     return None, None, None
 
-def drone_takeoff(drone):
-    print("Leader Drone Taking off...")
+def drone_takeoff(drone, target_system):
+    if target_system == 1:
+        print("Taking off Leader Drone...")
+    elif target_system > 1:
+        print("Taking off Follower Drone...")
+    current_position = drone_position(drone)
+
     drone.mav.command_long_send(
-        1,
+        target_system,
         0,
         mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,
         0,
-        0, 0, 0, 0, 36.010550, 129.321334, 30
+        0, 0, 0, 0, 36.010550, 129.321334, 10
     )
-    print("Leader Drone Takeoff!")
 
     current_position = drone_position(drone)
     while True:
         current_position = drone_position(drone)
-        if abs(current_position[2] - 30) > 1:
+        if abs(current_position[2] - 10) > 1:
             continue
         else:
+            if target_system == 1:
+                print("Leader Drone Took off!")
+            elif target_system > 1:
+                print("Follower Drone Took off!")
             time.sleep(2)
             return
 
-def drone_land(drone):
-    print("Leader Drone Landing...")
+def drone_land(drone, target_system):
+    if target_system == 1:
+        print("Landing Leader Drone...")
+    elif target_system > 1:
+        print("Landing Follower Drone...")
+    current_position = drone_position(drone)
+
     drone.mav.command_long_send(
-        1,
+        target_system,
         0,
         mavutil.mavlink.MAV_CMD_NAV_LAND,
         0,
-        0, 0, 0, 0, 36.010550, 129.321334, 0
+        0, 0, 0, 0, lat, lon, target_alt
     )
-    print("Leader Drone Land!")
 
     current_position = drone_position(drone)
     while True:
@@ -92,18 +128,66 @@ def drone_land(drone):
         if abs(current_position[2] - 0) > 1:
             continue
         else:
+            if target_system == 1:
+                print("Leader Drone Landed!")
+            elif target_system > 1:
+                print("Follower Drone Landed!")
             time.sleep(2)
             return
 
+def calculate_position(leader_lat, leader_lon, leader_alt, distance):
+    R = 6371e3
+    dlat = distance / R
+
+    target_lat = leader_lat - (dlat * 180 / math.pi)
+    target_lon = leader_lon
+    target_alt = leader_alt
+    return target_lat, target_lon, target_alt
+
+def send_command(follower, lat, lon, alt):
+    follower.mav.set_position_target_global_int_send(
+        0,
+        2,
+        0,
+        mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT_INT,
+        0b0000111111111000,
+        int(lat * 1e7), int(lon * 1e7), alt,
+        0, 0, 0,
+        0, 0, 0,
+        float('nan'), float('nan')
+    )
+
 def main():
     leader = drone_connect(14540)
+    follower = drone_connect(14541)
 
-    drone_arm(leader)
-    drone_takeoff(leader)
-    drone_land(leader)
-    drone_disarm(leader)
+    drone_arm(leader, target_system=1)
+    drone_arm(follower, target_system=2)
 
-    leader.close()
+    drone_takeoff(leader, target_system=1)
+    drone_takeoff(follower, target_system=2)
+
+    follower_set_mode(follower, target_system=2)
+
+    while True:
+        leader_position = drone_position(leader)
+
+        target_lat, target_lon, target_alt = calculate_position(leader_position[0], leader_position[1], leader_position[2], distance=2.0)
+
+        send_command(follower, target_lat, target_lon, target_alt)
+
+        time.sleep(0.1)
+
+
+    # drone_land(leader, target_system=1)
+    # drone_land(follower, target_system=2)
+
+    # drone_disarm(leader, target_system=1)
+    # drone_disarm(follower, target_system=2)
+
+    # leader.close()
+    # follower.close()
 
 if __name__ == "__main__":
     main()
+
